2824fd8872aa541a2e837893f1387136f8c6fbe5
Félic VIART <viart.felix@gmail.com>
2025-04-04 16:26:01 +0200

    Added auth middleware, signin and login routes

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,118 +1,103 @@
 const jwt = require("jsonwebtoken");
 const User = require("./../model/user.model.js");
 const bcrypt = require("bcryptjs");
+const { areObjectFieldsPresent } = require("../utils");
 
-exports.signin = async (req, res) => {
-    if (!req.body.email || req.body.email === "" || !req.body.password || req.body.password === "") {
-        return res.status(400).json({ message: "Veuillez saisir un email et un mot de passe" })
-    };
-    
-    let user = await User.create({
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 10)
-    });
+const exporter = {}
 
-    res.status(201).json(user);
-}
-
-exports.login = async (req, res) => {
-    if (!req.body.email || req.body.email === "" || !req.body.password || req.body.password === "") {
-        return res.status(400).json({ message: "login ou mot de passe incorrect" })
-    };
-    let user = await User.findOne({ email: req.body.email });
-    if (!user) {
-        return res.status(401).json({ error: "login ou mot de passe incorrect" });
-    }
-    if (!bcrypt.compareSync(req.body.password, user.password)) {
-        return res.status(401).json({ error: "login ou mot de passe incorrect" });
-    }
-    return res.status(200).json({
-        _id: user._id,
-        email: user.email,
-        token: jwt.sign({
-            _id: user._id,
-            email: user.email
-        }, process.env.JWT_KEY, { expiresIn: '24H' })
-    });
-}
-
-exports.getAll = async (req, res) => {
-    let userList = await User.find();
-    if (!userList) {
-        return res.status(404).json({ error: "utilisateurs non trouvés" });
-    }
-    return res.status(200).json(userList);
-}
-
-exports.getById = async (req, res) => {
-    let user = await User.findOne({ _id: req.params.id });
-    if (!userList) {
-        return res.status(404).json({ error: "utilisateur non trouvé" });
-    }
-    res.status(200).json(user);
-}
-
-exports.update = async (req, res) => {
-    let user = await User.findOne({ _id: req.token._id });
-    if (!user) {
-        return res.status(404).json({ error: "L'utilisateur n'existe pas" });
-    }
-    if (req.body.email) {
-        user.email = req.body.email;
-    }
-    if (req.body.password) {
-        user.password = bcrypt.hashSync(req.body.password, 10);
-    }
+/**
+ * Registers a new user to the db.
+ * @param req
+ * @param res
+ * @returns {Promise<*>}
+ */
+exporter.signin = async (req, res) => {
     try {
-        await user.save();
-        return res.status(201).json(user);
-    }catch(e){
-        return res.status(500).json({error: "Problème lors de la sécurisation du mot de passe"})
+        const requestBody = req.body
+
+        if( !areObjectFieldsPresent(requestBody, ['email', 'password', 'pseudo']) ) {
+            return res.status(400).json({ message: "Please enter an email, pseudo and password." })
+        }
+
+        const createdUser = await User.create({
+            email: requestBody.email,
+            pseudo: requestBody.pseudo,
+            password: bcrypt.hashSync(requestBody.password, 10)
+        })
+
+        const userAsObject = {
+            pseudo: createdUser.pseudo,
+            email: createdUser.email,
+            _id: createdUser._id
+        }
+
+        return res.status(201).json({
+            user: userAsObject
+        })
+    } catch (e) {
+        console.log(e)
+
+        return res.status(500).json({
+            message: "Server Error when signing in."
+        })
     }
 }
 
-exports.updateAdmin = async (req, res) => {
-    let user = await User.findOne({ _id: req.params.id });
-    if (!user) {
-        return res.status(404).json({ error: "L'utilisateur n'existe pas" });
-    }
-    if (req.body.email) {
-        user.email = req.body.email;
-    }
-    if (req.body.password) {
-        user.password = bcrypt.hashSync(req.body.password, 10);
-    }
+/**
+ * Attempts to log in the user. If credentials are correct, the jwt token and some user infos
+ * will be returned into the request.
+ * @param req
+ * @param res
+ * @returns {Promise<*>}
+ */
+exporter.login = async (req, res) => {
     try {
-        await user.save();
-        return res.status(201).json(user);
-    }catch(e){
-        return res.status(500).json({error: "Problème lors de la sécurisation du mot de passe"})
+        const requestBody = req.body
+
+        const badCredentialsMessage = "Please enter a pseudo and a password."
+
+        if( !requestBody.pseudo || requestBody.pseudo === "" || !requestBody.password || requestBody.password === "" ) {
+            return res.status(401).json({ message: badCredentialsMessage })
+        }
+
+        const targetUser = await User.findOne({
+            pseudo: requestBody.pseudo
+        })
+
+        if( !targetUser ) {
+            return res.status(401).json({ message: badCredentialsMessage })
+        }
+
+        if( !bcrypt.compareSync(requestBody.password, targetUser.password) ) {
+            return res.status(401).json({ message: badCredentialsMessage })
+        }
+
+        const userInfos = {
+            email: targetUser.email,
+            pseudo: targetUser.pseudo,
+            _id: targetUser._id
+        }
+
+        const token = jwt.sign(
+            userInfos,
+            process.env.JWT_KEY,
+            {
+                expiresIn: "1H"
+            }
+        )
+
+        return res.status(200).json({
+            user: userInfos,
+            jwt: token
+        })
+
+    } catch (e) {
+        console.log(e)
+
+        return res.status(500).json({
+            message: "Server Error when logging in."
+        })
     }
 }
 
-exports.update = async (req, res) => {
-    let user = await User.findOne({ _id: req.params.id });
-    if (!user) {
-        return res.status(404).json({ error: "L'utilisateur n'existe pas" });
-    }
-    if (req.body.email) {
-        user.email = req.body.email;
-    }
-    if (req.body.password) {
-        user.password = bcrypt.hashSync(req.body.password, 10);
-    }
-    try {
-        await user.save();
-        return res.status(201).json(user);
-    }catch(e){
-        return res.status(500).json({error: "Problème lors de la sécurisation du mot de passe"})
-    }
-}
-
-exports.delete = async (req, res) => {
-    let result = await User.deleteOne({ _id: req.params.id });
-    if (result !== 1) {
-        return res.status(404).json({ error: "utilisateur non trouvé" });
-    }
-    return res.status(200).json({ message: "Utilisateur supprimé" });
-}
+module.exports = exporter
