695cd5a37db9803429ab49e6203c91bba77bba01
Hugo <h.ilarraz@digitalprismaplayers.com>
2025-04-04 17:03:36 +0200

    feat: add authentication middleware to comment creation and deletion routes

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const commentController = require('./../controller/comment.controller.js');
+const authMiddleware = require('./../middleware/auth.middleware.js');
 
 router.post('/', authMiddleware, commentController.create);
 router.delete('/:id', authMiddleware, commentController.delete);
