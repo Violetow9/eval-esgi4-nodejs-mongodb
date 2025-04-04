fdce0d21a678e0be0c72e271c7074fedd7c4b76b
Hugo <h.ilarraz@digitalprismaplayers.com>
2025-04-04 16:36:52 +0200

    fix: add authentication middleware to post routes for enhanced security

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const postController = require('./../controller/post.controller.js');
+const authMiddleware = require('./../middleware/auth.middleware.js');
 
-router.post('/', postController.create);
-router.put('/:id', postController.update);
+router.post('/', authMiddleware, postController.create);
+router.put('/:id', authMiddleware, postController.update);
 router.get('/', postController.getAll);
-router.delete('/:id', postController.delete);
+router.delete('/:id', authMiddleware, postController.delete);
 
 module.exports = router;
