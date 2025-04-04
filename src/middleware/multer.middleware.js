6730927a6a68cdfc040c03e395380d68bdb93115
Hugo <h.ilarraz@digitalprismaplayers.com>
2025-04-04 15:48:39 +0200

    feat: add multer middleware for handling file uploads

---
+++ src/middleware/multer.middleware.js
@@ -0,0 +1,18 @@
+const multer = require('multer');
+
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png',
+};
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'picture')
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname.split(' ').join('_') + Date.now() + "." + MIME_TYPES[file.mimetype]);
+    }
+});
+
+module.exports = multer({storage: storage}).single('picture');
