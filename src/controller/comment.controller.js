9fd1056044ae1bfee501ae920b2e13a30bde3074
KoZeuh <kozeuhdev@gmail.com>
2025-04-04 16:11:37 +0200

    Refactor logic "comment" controller

--- src/controller/comment.controller.js
+++ src/controller/comment.controller.js
@@ -5,53 +5,80 @@ exports.create = async (req, res) => {
         return res.status(400).json({ message: "Veuillez saisir un commentaire" })
     };
 
-    let comment = await Comment.create({
-        text: req.body.text,
-        user_id: req.token._id,
-        post_id: req.params.postId
-    });
-
-    if (!comment) {
-        return res.status(500).json({ error: "Erreur lors de la création du commentaire" });
+    if (!req.params.postId) {
+        return res.status(400).json({ message: "Veuillez spécifier un post" })
     }
 
-    res.status(201).json(comment);
+    let comment = {
+        "text": req.body.text,
+        "user_id": req.token._id,
+        "post_id": req.params.postId,
+        "created_at": Date.now(),
+        "updated_at": Date.now()
+    };
+
+    try {
+        await Comment.create(comment);
+        return res.status(201).json(comment);
+    } catch (e) {
+        return res.status(500).json({ error: "Erreur lors de la création du commentaire" });
+    }
 }
 
 exports.update = async (req, res) => {
-    let comment = await Comment.findOne({ _id: req.params.commentId });
+    const commentId = req.params.commentId;
+
+    if (!commentId) {
+        return res.status(400).json({ error: "Vous devez spécifier un id de commentaire" });
+    }
+
+    const comment = await Comment.findOne({ _id: commentId });
 
     if (!comment) {
         return res.status(404).json({ error: "Commentaire non trouvé" });
     }
 
-    if (comment.user_id.toString() !== req.token._id) {
-        return res.status(403).json({ error: "Vous n'êtes pas autorisé à modifier ce commentaire" });
+    const newText = req.body.text;
+    if (newText) {
+        comment.text = newText;
+        comment.updated_at = Date.now();
     }
 
-    comment.text = req.body.text || comment.text;
-    comment.updated_at = Date.now();
-
-
-    await comment.save();
-
-    return res.status(200).json(comment);
+    try {
+        await comment.save();
+        return res.status(201).json({
+            message: "Commentaire mis à jour",
+            comment: comment
+        });
+    } catch (e) {
+        return res.status(500).json({ error: "Erreur lors de la mise à jour du commentaire" });
+    }
 }
 
 exports.delete = async (req, res) => {
-    let comment = await Comment.findOne({ _id: req.params.commentId });
+    const commentId = req.params.commentId;
+
+    if (!commentId) {
+        return res.status(400).json({ error: "Vous devez spécifier un id de commentaire" });
+    }
+    
+    const comment = await Comment.findOne({ _id: commentId });
 
     if (!comment) {
         return res.status(404).json({ error: "Commentaire non trouvé" });
     }
 
-    if (comment.user_id.toString() !== req.token._id) {
-        return res.status(403).json({ error: "Vous n'êtes pas autorisé à supprimer ce commentaire" });
+    if (comment.user_id.toString() !== req.token._id.toString()) {
+        return res.status(403).json({ error: "Vous ne pouvez pas supprimer ce commentaire" });
     }
 
-    await Comment.deleteOne({ _id: req.params.commentId });
+    const result = await Comment.deleteOne({_id: commentId});
 
-    return res.status(200).json({ message: "Commentaire supprimé" });
+    if (result !== 1) {
+        return res.status(404).json({error: "Une erreur est survenue lors de la suppression du commentaire"});
+    }
+    
+    return res.status(200).json({message: "Commentaire supprimé"});
 }
 
 
