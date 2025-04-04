edac8be673b120346e17e45932b6d25b11e55115
Hugo <h.ilarraz@digitalprismaplayers.com>
2025-04-04 16:36:37 +0200

    fix: simplify authentication middleware and improve error messages

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,24 +1,21 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = (role) => {
-    return (req, res, next) => {
-        const token = req.headers?.authorization?.split(" ")[1];
+module.exports = (req, res, next) => {
+    const token = req.headers?.authorization?.split(" ")[1];
 
-        if (!token) {
-            res.status(401).json({
-                'message': 'You must be logged in to do this action.'
-            });
-            return;
-        }
-
-        req.token = jwt.verify(token, process.env.JWT_KEY);
-
-        if (!req.token) {
-            res.status(401).json({
-                'message': 'You must be logged in to do this action.'
-            });
-            return;
-        }
-        
+    if (!token) {
+        return res.status(401).json({
+            'message': "Vous devez indiquer un token d'authentification"
+        });
     }
+
+    req.token = jwt.verify(token, process.env.JWT_KEY);
+
+    if (!req.token) {
+        res.status(401).json({
+            'message': 'Vous devez être connecté pour faire cette action.'
+        });
+    }
+
+    next();
 }
